372bd96d4e5058082e5ee9f104e834b239427d47
John F <jforsythe13@qub.ac.uk>
2019-08-28 20:54:51 +0100

    Junit test cases created

---
+++ The ATM App/test/theATMApp/ATMTest.java
@@ -0,0 +1,37 @@
+package theATMApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ATMTest {
+	// test data
+	int amount1, amount2, amount3;
+
+	@Before
+	public void setUp() throws Exception {
+		// Initialise the data
+		amount1 = 0;
+		amount2 = 221;
+		amount3 = 321;
+	}
+	
+	@Test
+	public void testselection() {
+		
+	}
+	
+	@Test
+	public void furtherWithdrawal() {
+		
+	}
+	
+	
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
