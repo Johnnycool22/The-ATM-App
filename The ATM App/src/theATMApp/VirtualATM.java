0e7788ff65e3b21e31ac512d210fc4a4067bcea8
John F <jforsythe13@qub.ac.uk>
2019-08-28 17:30:37 +0100

    updates to overdraft

--- The ATM App/src/theATMApp/VirtualATM.java
+++ The ATM App/src/theATMApp/VirtualATM.java
@@ -2,21 +2,25 @@ package theATMApp;
 
 import java.util.Scanner;
 
-public class VirtualATM {
+public class VirtualATM extends NotesType {
 
 	private static Scanner input;
 	// current balance set to £220
 	private static double balance = 220;
 	private static double overdraft = 100;
-	private static int anotherWithdrawal;
 
 	public static void main(String[] args) throws IllegalArgumentException {
 		input = new Scanner(System.in);
-
+		// methods statements used
 		selection();
-		
+		withdrawal();
+		furtherWithdrawal();
+
 	}
 
+	/**
+	 * Switch statement method to select main ATM options
+	 */
 	private static void selection() {
 		int selection;
 
@@ -31,25 +35,29 @@ public class VirtualATM {
 		case 1:
 			System.out.println("Your current balance is: £" + balance + "\n");
 			System.out.println("You have an overdraft limit of: £" + overdraft + "\n");
-			anotherWithdrawal();
+			withdrawal();
 			break;
 
 		case 2:
 			float amount;
 			System.out.println("Please enter your cash withdrawal amount: ");
 			amount = input.nextFloat();
-			if (amount >= balance + overdraft || amount == 0) {
+			if (amount > balance) {
+				System.out.println("You have overdrawn!! £100 limit allowable\n\n");
+				withdrawal();
+			} else if (amount > balance + overdraft) {
 				System.out.println("You have exceeded your overdraft limit!\n\n");
-				anotherWithdrawal();
+				withdrawal();
 			} else {
 				balance = balance - amount;
 				System.out.println("You have withdrawn £" + amount + ", your new balance is: £" + balance + "\n");
 				System.out.println("Please collect your money!\n");
-				anotherWithdrawal();
+				withdrawal();
 			}
 			break;
 
 		case 3:
+			System.out.println("Your ATM session has ended, please remove your card!");
 			System.exit(0);
 
 		default:
@@ -60,17 +68,49 @@ public class VirtualATM {
 
 	}
 
-	private static void anotherWithdrawal() {
-		System.out.println("Would you like to make a cash withdrawal?\n\nPlease Select:");
-		System.out.println("1. Cash Withdrawal\n2. EXIT");
-		anotherWithdrawal = input.nextInt();
-		if (anotherWithdrawal == 1) {
-			selection();
-		} else if (anotherWithdrawal == 2) {
-			System.out.println("Your ATM session has ended, please remove your card!");
-		} else {
-			System.out.println("Invalid selection\n\n");
-			anotherWithdrawal();
+	private static void withdrawal() {
+		System.out.println("Would you like to make a cash withdrawal?\n");
+		furtherWithdrawal();
+	}
+
+	/**
+	 * Another Switch statement method to select a further cash withdrawal
+	 */
+	private static void furtherWithdrawal() {
+		int selection;
+
+		System.out.println("1. Cash Withdrawal");
+		System.out.println("2. EXIT");
+		System.out.println("Please select an option you wish to perform:");
+
+		selection = input.nextInt();
+
+		switch (selection) {
+
+		case 1:
+			float amount;
+			System.out.println("Please enter your cash withdrawal amount: ");
+			amount = input.nextFloat();
+			if (amount >= balance + overdraft || amount == 0) {
+				System.out.println("You have exceeded your overdraft limit!\n\n");
+				withdrawal();
+			} else {
+				balance = balance - amount;
+				System.out.println("You have withdrawn £" + amount + ", your new balance is: £" + balance + "\n");
+				System.out.println("Please collect your money!\n");
+				withdrawal();
+			}
+			break;
+
+		case 2:
+			System.out.println("Your ATM session has now ended, please remove your card!");
+			System.exit(0);
+
+		default:
+			System.out.println("Invalid selection:\n\n");
+			withdrawal();
+			break;
 		}
+
 	}
 }
