70fd061f04bc501c08659a26544966d3aefd5337
John F <jforsythe13@qub.ac.uk>
2019-08-29 17:06:44 +0100

    UX content added to both apps

--- The ATM App/src/theATMApp/NotesType.java
+++ The ATM App/src/theATMApp/NotesType.java
@@ -2,20 +2,41 @@ package theATMApp;
 
 import java.io.*;
 
-class NotesType {
+public class NotesType  {
+		// current balance set to £220 and overdraft £100
+		private static double balance = 220;
+		private static double overdraft = 100;
+	
 	public static void main(String args[]) throws IOException {
+		// Welcome header
+		System.out.println("*******************************");
+		System.out.println("**  Welcome to The ATM App!  **");
+		System.out.println("*******************************");
+		System.out.println("**Lets get withdrawing some cash!!**\n");
+		
 		// create object buffer class
 		BufferedReader cashAmount = new BufferedReader(new InputStreamReader(System.in));
 
 		// ATM machine denominations available into an array
-		int Denom[] = { 20, 20, 20, 20, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5, 5, 5,
+		int Notes[] = { 20, 20, 20, 20, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5, 5, 5,
 				5 };
+		
+		System.out.println("Your current balance is: £"+balance+"\n");
+		System.out.println("You have an overdraft limit of: £" + overdraft + "\n");
+		System.out.println("-------------------------------------------\n");
 
 		//  enter cash amount you require
-		System.out.print("Please enter your cash withdrawal amount: ");
+		System.out.print("Please enter your cash withdrawal amount: £");
 
 		// store amount entered
 		int amount = Integer.parseInt(cashAmount.readLine());
+		if (amount > balance || amount > balance + overdraft) {
+			System.out.println("You have overdrawn!! £100 maximum allowable\n\n");
+		} else {
+			balance = balance - amount;
+			System.out.println("\nYou have withdrawn £" + amount + ", your new balance is: £" + balance + "\n");
+			System.out.println("**Please collect your money!**\n");
+		}
 
 		// create copy of the amount
 		int copy = amount;
@@ -26,23 +47,24 @@ class NotesType {
 		// check for notes
 		for (int i = 0; i < 26; i++) {
 			// count quantity of notes.
-			count = amount / Denom[i];
+			count = amount / Notes[i];
 			if (count != 0) {
-				System.out.println(Denom[i] + "\tx\t" + count + "\t= " + Denom[i] * count);
+				System.out.println("£"+Notes[i] + "\tx\t" + count + "\t= £" + Notes[i] * count);
 			}
 			// finds total number of notes
 			totalNotes = totalNotes + count; 
 			// finds the remaining number denominations
-			amount = amount % Denom[i];
+			amount = amount % Notes[i];
 		}
 
 		System.out.println("--------------------------------");
 
 		// displays the total amount
-		System.out.println("TOTAL\t\t\t= " + copy);
+		System.out.println("TOTAL\t\t\t= £" + copy);
 		System.out.println("--------------------------------");
 
 		// displays the total number of notes
 		System.out.println("Total Number of Notes\t= " + totalNotes);
 	}
+	
 }
--- The ATM App/src/theATMApp/VirtualATM.java
+++ The ATM App/src/theATMApp/VirtualATM.java
@@ -8,26 +8,34 @@ public class VirtualATM extends NotesType {
 	// current balance set to £220
 	private static double balance = 220;
 	private static double overdraft = 100;
+	NotesType notes = new NotesType();
 
 	public static void main(String[] args) throws IllegalArgumentException {
+		// Welcome header
+		System.out.println("*******************************");
+		System.out.println("**  Welcome to The ATM App!  **");
+		System.out.println("*******************************");
+		System.out.println("**Lets get withdrawing some cash!!**\n");
+		
 		input = new Scanner(System.in);
 		// methods statements used
 		selection();
 		withdrawal();
 		furtherWithdrawal();
-
 	}
 
 	/**
 	 * Switch statement method to select main ATM options
 	 */
 	private static void selection() {
+		
 		int selection;
 
+		System.out.println("Please select an option you wish to perform:");
 		System.out.println("1. Check Balance");
 		System.out.println("2. Cash Withdrawal");
 		System.out.println("3. EXIT");
-		System.out.println("Please select an option you wish to perform:");
+		
 
 		selection = input.nextInt();
 
@@ -35,6 +43,7 @@ public class VirtualATM extends NotesType {
 		case 1:
 			System.out.println("Your current balance is: £" + balance + "\n");
 			System.out.println("You have an overdraft limit of: £" + overdraft + "\n");
+			System.out.println("-------------------------------------------\n");
 			withdrawal();
 			break;
 
@@ -48,13 +57,13 @@ public class VirtualATM extends NotesType {
 			} else {
 				balance = balance - amount;
 				System.out.println("You have withdrawn £" + amount + ", your new balance is: £" + balance + "\n");
-				System.out.println("Please collect your money!\n");
+				System.out.println("**Please collect your money!**\n");
 				withdrawal();
 			}
 			break;
 
 		case 3:
-			System.out.println("Your ATM session has ended, please remove your card!");
+			System.out.println("**Your ATM session has ended, please remove your card!**");
 			System.exit(0);
 
 		default:
@@ -75,10 +84,10 @@ public class VirtualATM extends NotesType {
 	 */
 	private static void furtherWithdrawal() {
 		int selection;
-
+		
+		System.out.println("Please select an option you wish to perform:");
 		System.out.println("1. Cash Withdrawal");
 		System.out.println("2. EXIT");
-		System.out.println("Please select an option you wish to perform:");
 
 		selection = input.nextInt();
 
@@ -88,13 +97,13 @@ public class VirtualATM extends NotesType {
 			float amount;
 			System.out.println("Please enter your cash withdrawal amount: ");
 			amount = input.nextFloat();
-			if (amount > balance + overdraft) {
+			if (amount > balance || amount > balance + overdraft) {
 				System.out.println("You have overdrawn!! £100 maximum allowable\n\n");
 				withdrawal();
 			} else {
 				balance = balance - amount;
 				System.out.println("You have withdrawn £" + amount + ", your new balance is: £" + balance + "\n");
-				System.out.println("Please collect your money!\n");
+				System.out.println("**Please collect your money!**\n");
 				withdrawal();
 			}
 			break;
